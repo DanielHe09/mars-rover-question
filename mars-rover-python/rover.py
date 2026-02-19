2726e3be6972704c83def69c2122617af5d88cd2
DanielHe09 <dandanhe09@gmail.com>
2026-02-19 15:22:09 -0500

    constructor for rover done

---
+++ mars-rover-python/rover.py
@@ -0,0 +1,8 @@
+class Rover:
+    def __init__(self, id: int, position_x: int, position_y: int, position_direction: str, instructions: str):
+        self.id = id
+        self.position_x = position_x
+        self.position_y = position_y
+        self.position_direction = position_direction
+        self.instructions = instructions
+
